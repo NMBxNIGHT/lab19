785791dff9c37baa9bdfbfa611dcb4614eb43f39
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-02-17 17:00:38 +0700

    เดียวมาต่อ

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -5,6 +5,7 @@
 #include<cstdlib>
 
 using namespace std;
+int count=0;
 
 char score2grade(int score){
     if(score >= 80) return 'A';
@@ -20,20 +21,70 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> name,vector<int> scores,vector<char> grade){
+  
+    ifstream file;
+    file.open(filename.c_str());
+    string data;
+    int score[3];
+    char format[]="%[^:]: %d %d %d",S_name[500]={};
+    
+    while(getline(file,data)){
+    sscanf(data.c_str(),format,S_name,&score[0],&score[1],&score[2]);
+    name.push_back(S_name);
+    scores.push_back(score[0]+score[1]+score[2]);
+    grade.push_back(score2grade(score[0]+score[1]+score[2]));
+    count++;
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+    string text;
+    cout <<"Please input your command: ";
+    getline(cin,text);
+ 
+    char format[]="%s %s";
+    char ss[50],bb[1000];
+    sscanf(text.c_str(),format,&ss,&bb);
+    command = ss;
+    key = bb;    
+    
 }
 
-void searchName(){
+void searchName(vector<string> names,vector<int> scores,vector<char> grade,string key){
+    unsigned int check=1;
+    cout << names.size();
+    for(unsigned int i=0;i<count;i++){
+        string key_check=toUpperStr(names[i]);
+        cout << key_check <<" ";
+        if(key==key_check){
+            cout << "ssssss";
+           // printf("%s's score = %d\n",names[i].c_str(),scores[i]); 
+          //  printf("%s's grade = %c\n",names[i].c_str(),grade[i]);
+        }else{
+            check++;
+        }
+    }
 
+    if(check == scores.size()){
+        cout<<"---------------------------------\n" << "Cannot found.\n" <<"---------------------------------\n";
+    }
 }
 
-void searchGrade(){
+void searchGrade(vector<string> names,vector<int> scores,vector<char> grade,string key){
+ unsigned int check=0;
+    for(unsigned int i=0;i<scores.size();i++){
+        string key_check=toUpperStr(names[i]);
+        if(key == key_check){
+         printf("%s (%d)\n",names[i].c_str(),scores[i]);
+        }else{
+            check++;
+        }
+    }
 
+    if(check == scores.size()){
+        cout<<"---------------------------------\n" << "Cannot found.\n" <<"---------------------------------\n";
+    }
 }
 
 
